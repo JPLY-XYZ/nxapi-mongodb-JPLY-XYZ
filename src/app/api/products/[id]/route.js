b4f4673fe1d198f48c65eb60ecbeab5463947300
JPLY-XYZ <josecabeza.compras@gmail.com>
2025-02-13 22:17:36 +0100

    TEST 4

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -1,41 +1,71 @@
 import { connectToDatabase } from "@/lib/mongodb";
-import { ObjectId } from "mongodb"; 
+import { ObjectId } from "mongodb";
 
 export async function GET(request, { params }) {
     const { database } = await connectToDatabase();
     const collection = database.collection(process.env.MONGODB_COLLECTION);
 
-    const { id } = await params
-    const results = await collection.find({ _id: new ObjectId(id) }).toArray()
+    const { id } = await params;
+    const results = await collection.find({ _id: new ObjectId(id) }).toArray();
 
-    return Response.json(results[0]);
+    return new Response(JSON.stringify(results[0]), {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+    });
 }
 
-
 export async function PUT(request, { params }) {
-    const content = request.headers.get('content-type')
+    const content = request.headers.get('content-type');
 
-    if (content != 'application/json')
-        return Response.json({ message: 'Debes proporcionar datos JSON' })
+    if (content != 'application/json') {
+        return new Response(JSON.stringify({ message: 'Debes proporcionar datos JSON' }), {
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+            }
+        });
+    }
 
     const { database } = await connectToDatabase();
     const collection = database.collection(process.env.MONGODB_COLLECTION);
 
-    const { id } = await params
-    const { nombre, descripcion, imagen, fecha_entrada } = await request.json() // Read body request
-    const results = await collection.updateOne({ _id: new ObjectId(id) }, { $set: { nombre, descripcion, imagen, fecha_entrada } });
+    const { id } = await params;
+    const { nombre, descripcion, imagen, fecha_entrada } = await request.json(); // Read body request
+    const results = await collection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { nombre, descripcion, imagen, fecha_entrada } }
+    );
 
-    return Response.json(results);
+    return new Response(JSON.stringify(results), {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+    });
 }
-
 
 export async function DELETE(request, { params }) {
     const { database } = await connectToDatabase();
     const collection = database.collection(process.env.MONGODB_COLLECTION);
 
-    const { id } = await params
-    const results = await collection.deleteOne({ _id: new ObjectId(id) })
+    const { id } = await params;
+    const results = await collection.deleteOne({ _id: new ObjectId(id) });
 
-    return Response.json(results);
+    return new Response(JSON.stringify(results), {
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+    });
 }
 
--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -6,21 +6,43 @@ export async function GET(request) {
 
     const results = await collection.find({}).toArray();
 
-    return Response.json(results);
+    return new Response(JSON.stringify(results), {
+        headers: { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+    });
 }
 
 export async function POST(request) {
-    const content = request.headers.get('content-type')
+    const content = request.headers.get('content-type');
 
-    if (content != 'application/json')
-        return Response.json({ message: 'Debes proporcionar datos JSON' })
+    if (content !== 'application/json') {
+        return new Response(JSON.stringify({ message: 'Debes proporcionar datos JSON' }), {
+            headers: { 
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+                'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+            }
+        });
+    }
 
     const { database } = await connectToDatabase();
-    const collection = database.collection(process.env.MONGODB_COLLECTION)
+    const collection = database.collection(process.env.MONGODB_COLLECTION);
 
-    const { nombre, descripcion, imagen, fecha_entrada } = await request.json() // Read body request
+    const { nombre, descripcion, imagen, fecha_entrada } = await request.json(); // Read body request
     const results = await collection.insertOne({ nombre, descripcion, imagen, fecha_entrada });
 
-    return Response.json(results)
+    return new Response(JSON.stringify(results), {
+        headers: { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+        }
+    });
 }
 
