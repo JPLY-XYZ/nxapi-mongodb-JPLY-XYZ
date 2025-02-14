10517fda44c4c43aa8088e82f1d0b15adc86ae4e
JPLY-XYZ <josecabeza.compras@gmail.com>
2025-02-14 16:50:23 +0100

    TEST 14

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -2,7 +2,7 @@ import { connectToDatabase } from "@/lib/mongodb";
 
 const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
