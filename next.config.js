ddeb304c7ab8601ec1594b041ce9ff031bea017c
JPLY-XYZ <josecabeza.compras@gmail.com>
2025-02-14 16:45:33 +0100

    TEST 12

--- next.config.js
+++ next.config.js
@@ -3,18 +3,5 @@ const nextConfig = {}
 
 module.exports = nextConfig
 
-// next.config.js
-module.exports = {
-    experimental: {
-      middlewareFile: true, // Habilita el uso de middleware en Next.js (para versiones anteriores)
-    },
-    async rewrites() {
-      return [
-        {
-          source: '/api/:path*', // Aplica el middleware a las rutas de la API
-          destination: '/:path*', // Permite que el middleware se aplique correctamente
-        },
-      ];
-    },
-  };
+
   
